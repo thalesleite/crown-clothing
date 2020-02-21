7835e6904c72729d0fffd6856e1ef0531d38747a
thalesleite <thalesaleite@gmail.com>
2020-02-21 02:53:45 +0000

    created sign-in page sign-out page form component form-input component and custom-button component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignOut from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
 import './pages/homepage/homepage.styles.scss';
@@ -15,7 +16,8 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
-      </Switch>
+        <Route path='/signin' component={SignInAndSignOut} />
+        </Switch>
     </div>
   );
 }
